3418362c3e4393291352db6b3a6d1a2adecd3c48
Han <jervois@Jervoiss-MacBook-Pro.local>
2017-05-23 15:59:34 +0800

    Im done

--- git-and-github-practice/script.js
+++ git-and-github-practice/script.js
@@ -1,1 +1,8 @@
+https://github.com/jmhs/lab-git-github.gitwindow.onload = function(){
+  var button = document.getElementById("button-yellow");
+  button.addEventListener("click", function(event){
+    prompt('What is your email?')
+  });
+}
+
 //Javascript to be added
